b76b3d426373283d31baa9f49cf09f986e10f283
keawade <keawade@gmail.com>
2015-10-25 00:09:17 -0500

    Styles updated

--- calculator.js
+++ calculator.js
@@ -1,20 +1,41 @@
-var buttons = [['7','8','9','+'],['4','5','6','-'],['1','2','3','*'],['0','.','/']];
+var buttons = [['7','8','9','+'],['4','5','6','-'],['1','2','3','*'],['0','.','=','/']];
 
-function addButtons(array){
-  var base = document.getElementById('calculator');
+function addBox(base){
+  var box = document.createElement('input');
+  box.className = 'cal-box';
+  base.appendChild(box);
+}
+
+function addButtons(base, array){
   var row, button, buttonContent;
   for(var i = 0; i < buttons.length; i++){
-    row = document.createElement('div');
+//    row = document.createElement('div');
     for(var j = 0; j < buttons[i].length; j++){
       button = document.createElement('button');
       buttonContent = document.createTextNode(array[i][j]);
       button.appendChild(buttonContent);
       button.className = 'cal-button';
-      row.appendChild(button);
+      base.appendChild(button);
     }
-    row.className = 'cal-row';
-    base.appendChild(row);
+//    row.className = 'cal-row';
+//    base.appendChild(row);
   }
 }
 
-document.body.onload = addButtons(buttons);
+function prep(){
+  var title = document.createElement('h1');
+  title.className = 'title';
+  title.textContent = 'Calculon';
+  document.body.appendChild(title);
+
+  var container = document.createElement('div');
+  container.id = 'calculator';
+  document.body.appendChild(container);
+
+  var baseDiv = document.getElementById('calculator');
+
+  addBox(baseDiv);
+  addButtons(baseDiv, buttons);
+}
+
+document.body.onload = prep();
